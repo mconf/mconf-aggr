a19e6a0cf0653f4f929b71453e79fd05632a4405
italo <italojmf@gmail.com>
2017-10-26 14:53:12 -0200

    Remove setups from main

--- evt_lst_main.py
+++ evt_lst_main.py
@@ -1,35 +1,14 @@
 #!/usr/bin/env python3.6
 
-
+import json
+import logging
 import time
+from urllib.parse import unquote
+
+import falcon
 
 import mconf_aggr.cfg as cfg
-from mconf_aggr.event_listener.db_operations import DataWritter
-from mconf_aggr.event_listener.webhooks_listener import DataReader, app
 from mconf_aggr.aggregator import Aggregator, SetupError, PublishError
-
-
-def main():
-    cfg.config.setup_config("config/config.json")
-
-    db_reader = DataReader()
-    db_writter = DataWritter()
-
-    aggregator = Aggregator()
-    aggregator.register_callback(db_writter, channel='webhooks')
-    
-    try:
-        aggregator.setup()
-    except SetupError:
-        exit(1)
-
-    publisher = aggregator.publisher
-
-    db_reader.setup(publisher)
-
-    # when?
-    #aggregator.stop()
-    #db_reader.stop()
-
-if __name__ == '__main__':
-    main()
+from mconf_aggr.event_listener import db_mapping
+from mconf_aggr.event_listener.db_operations import DataWritter
+from mconf_aggr.event_listener.webhooks_listener import app
