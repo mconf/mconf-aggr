ac0ae8c8de303d043fae6846e7d8ff8d07c222a0
italo <italojmf@gmail.com>
2017-10-26 14:52:26 -0200

    Move setups to webhooks_listener and decode python3 string to utf-8

--- mconf_aggr/event_listener/webhooks_listener.py
+++ mconf_aggr/event_listener/webhooks_listener.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python3.6
+
 import json
+import logging
+import time
+from urllib.parse import unquote
 
 import falcon
 
-from mconf_aggr import cfg
+import mconf_aggr.cfg as cfg
 from mconf_aggr.event_listener import db_mapping
+from mconf_aggr.event_listener.db_operations import DataWritter
+from mconf_aggr.aggregator import Aggregator, SetupError, PublishError
+
 
 # Falcon follows the REST architectural style, meaning (among
 # other things) that you think in terms of resources and state
@@ -12,12 +20,12 @@ class HookListener(object):
     def on_post(self, req, resp):
         # TODO: Treat when receiving multiple events on POST
         """Handles POST requests"""
-        DataReader.read(req)
+        # Parse received message
+        post_data = req.stream.read().decode('utf-8')
+
+        db_reader.read(post_data)
 
         resp.status = falcon.HTTP_200  # This is the default status
-
-# falcon.API instances are callable WSGI apps
-app = falcon.API()
 
 
 class DataReader():
@@ -37,11 +45,8 @@ class DataReader():
 
     def read(self, data):
         # TODO: Validade checksum
-        # Parse received message
-        post_data = data.stream.read()
-
         # Message will be in format event={data}&timestamp=BigInteger and encoded
-        decoded_data = urllib.unquote_plus(post_data)
+        decoded_data = unquote(data)
         decoded_data = decoded_data.split('&')
 
         # Set {data} in event={data} to events variable
@@ -55,6 +60,31 @@ class DataReader():
 
             if(mapped_msg):
                 try:
-                    self.publisher.publish(webhook_msg, mapped_msg, channel='webhooks')
+                    data = [webhook_msg, mapped_msg]
+                    self.publisher.publish(data, channel='webhooks')
                 except PublishError as err:
                     continue
+
+cfg.config.setup_config("config/config.json")
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
+
+db_reader = DataReader()
+db_writter = DataWritter()
+
+aggregator = Aggregator()
+aggregator.register_callback(db_writter, channel='webhooks')
+
+try:
+    aggregator.setup()
+except SetupError:
+    exit(1)
+
+publisher = aggregator.publisher
+
+db_reader.setup(publisher)
+
+# when?
+#aggregator.stop()
+#db_reader.stop()
