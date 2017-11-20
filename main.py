3a9330c541c25f823a3543f188b83c105aab62b1
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2017-11-20 17:36:13 -0200

    Adjust main.py to handle stopping aggregator.

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3.6
 
 
+import argparse
 import logging
 import time
+import sys
+
+import sqlalchemy as sa
 
 import mconf_aggr.cfg as cfg
 from mconf_aggr.zabbix.zabbix import ZabbixDataWriter, ZabbixDataReader
-from mconf_aggr.aggregator import Aggregator, SetupError, PublishError
+from mconf_aggr.aggregator import Aggregator, SetupError, PublishError, AggregatorStopped
 
 
 def main():
-    cfg.config.setup_config("config/config.json")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", help="custom JSON configuration file")
+    args = parser.parse_args()
+
+    if args.config:
+        cfg.config.setup_config(args.config)
+
     cfg.config.setup_logging()
 
     logger = logging.getLogger(__name__)
@@ -25,13 +35,15 @@ def main():
     try:
         aggregator.setup()
     except SetupError:
-        exit(1)
+        sys.exit(1)
 
     zabbix_reader.setup()
 
     publisher = aggregator.publisher
 
     period = cfg.config.zabbix['period']
+
+    aggregator.start()
 
     while True:
         try:
@@ -40,8 +52,17 @@ def main():
             if data: # What to publish when fetching data fails?
                 try:
                     publisher.publish(data, channel='zabbix')
-                except PublishError as err:
+                except PublishError:
+                    logger.warn("Something went wrong while publishing.")
+
                     continue
+                except AggregatorStopped:
+                    logger.info("Aggregator stopped.")
+
+                    zabbix_reader.stop()
+                    logger.info("Application finished due some failure. "
+                                "Exit status 1.")
+                    sys.exit(1)
 
             time.sleep(period)
         except:
@@ -49,6 +70,8 @@ def main():
 
     aggregator.stop()
     zabbix_reader.stop()
+
+    logger.info("Application finished with success. Exit status 0.")
 
 if __name__ == '__main__':
     main()
