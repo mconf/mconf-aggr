2a33cffbb86ec5decc993b79b8055371b584ef31
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2017-11-23 16:44:36 -0200

    Add better exception handler to Zabbix pool and server.

--- mconf_aggr/zabbix/zabbix.py
+++ mconf_aggr/zabbix/zabbix.py
@@ -148,7 +148,7 @@ class ServersPool:
             try:
                 server.connect()
             except ZabbixLoginError:
-                self.logger.exception(
+                self.logger.warn(
                     "Login to server {} has failed. Removing it from server pool."
                     .format(server)
                 )
@@ -166,6 +166,9 @@ class ServersPool:
 
     def __iter__(self):
         return iter(self.servers)
+
+    def __bool__(self):
+        return bool(self.servers)
 
     def __repr__(self):
         return "{!s}(servers={})".format(self.__class__.__name__,
@@ -218,15 +221,16 @@ class ZabbixServer:
         try:
             self.logger.debug("Login to server {}.".format(self))
             self.connection.login(self.login, self.password)
-        except api.ZabbixAPIException:
-            self.logger.exception(
-                "Something went wrong while trying to login to server {}."
-                .format(self)
+        except api.ZabbixAPIException as err:
+            self.logger.error(
+                "Something went wrong while trying to login to server {}: {}."
+                .format(self, err)
             )
             self.connection = None
             self._ok = False
 
-            raise ZabbixLoginError("Can not login to server".format(self))
+            raise ZabbixLoginError("Cannot login to server {}.".format(self)) \
+                from err
         else:
             self._ok = True
 
@@ -680,7 +684,7 @@ class ZabbixDataReader():
                 )
                 self.pool.remove_server(server)
             except Exception:
-                self.logger.exception(
+                self.logger.error(
                     "Something went wrong when getting hosts for server {}."
                     .format(server)
                 )
@@ -740,6 +744,10 @@ class ZabbixDataReader():
         # Here comes the logic to read data from Zabbix API.
         # It iterates over all Zabbix servers reading a subset of the
         # monitored items.
+        if not self.pool:
+            self.logger.warn("No servers in pool. Nothing to fetch.")
+            return None
+
         self.logger.debug("Fetching data from server pool.")
         application = cfg.config.zabbix['application']
         parameters = {"output": ["hostid", "name", "lastvalue"],
