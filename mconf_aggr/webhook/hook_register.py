0869a62147961b3bac88c71dc83cbafbd08cfbed
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2018-10-02 19:17:17 -0300

    Refactor webhook auto registration

--- mconf_aggr/webhook/hook_register.py
+++ mconf_aggr/webhook/hook_register.py
@@ -2,61 +2,19 @@ import hashlib
 import logging
 import requests
 import urllib.parse
-import xml.etree.ElementTree as ET
 from urllib.parse import urljoin
+from xml.etree import ElementTree
 
 class WebhookRegister:
-    class Server:
-        def __init__(self, server, secret, logger=None):
-            self._server = server
-            self._secret = secret
-            self.logger = logger or logging.getLogger(__name__)
-
-        def create_hook(self, callback_url, get_raw=False, hook_id=None):
-            hook_url = self._build_create_hook_url()
-            params = {"callbackURL": callback_url, "getRaw": get_raw}
-            if hook_id:
-                params["hookID"] = hook_id
-
-            checksum = hashlib.sha1("hooks/create".encode("utf-8") + urllib.parse.urlencode(params).encode("utf-8") + self._secret.encode("utf-8"))
-            params["checksum"] = checksum.hexdigest()
-
-            r = None
-            try:
-                r = requests.get(hook_url, params=params)
-            except requests.exceptions.ConnectionError as err:
-                self.logger.warn(err)
-            else:
-                if r.status_code == requests.codes.ok:
-                    import xml.etree.ElementTree as ET
-
-                    response_xml = ET.fromstring(r.text)
-                    return_code = response_xml.find("returncode").text
-
-                    if return_code == "SUCCESS":
-                        self.logger.info("Webhook registered successfuly to '{}'".format(self._server))
-                        print("Webhook registered successfuly to '{}'".format(self._server))
-                    else:
-                        self.logger.warn("Webhook failed to register to '{}'".format(self._server))
-                        print("Webhook failed to register to '{}'".format(self._server))
-                else:
-                    self.logger.warn("Webhook failed to register to '{}'".format(self._server))
-                    print("Webhook failed to register to '{}'".format(self._server))
-
-                    raise HookCreateError()
-
-            return r
-
-        def _build_create_hook_url(self):
-            return urljoin(self._server, "bigbluebutton/api/hooks/create")
-
-    def __init__(self, servers, callback_url, get_raw=False, hook_id=None):
+    def __init__(self, servers, callback_url, get_raw=False, hook_id=None, logger=None):
         self._servers = servers
         self._callback_url = callback_url
         self._get_raw = get_raw
         self._hook_id = hook_id
         self._success_servers = []
         self._failed_servers = []
+
+        self.logger = logger or logging.getLogger(__name__)
 
     @property
     def servers(self):
@@ -72,14 +30,81 @@ class WebhookRegister:
 
     def create_hooks(self):
         for server, token in self._servers.items():
-            inner_server = self.Server(server, token)
+            inner_server = WebhookServer(server, token)
             try:
                 _ = inner_server.create_hook(self._callback_url, self._get_raw, self._hook_id)
             except HookCreateError as err:
+                self.logger.warn(f"Webhook registration for server '{server}' failed.")
+
+                self.failed_servers.append(server)
+            except Exception as err:
+                self.logger.warn(f"Webhook registration for server '{server}' failed.")
+                self.logger.warn("An exceptional error occurred when trying to register webhooks.")
+                self.logger.debug(err)
+
                 self.failed_servers.append(server)
             else:
+                self.logger.info(f"Webhook registration for server '{server}' ok.")
+
                 self.success_servers.append(server)
+
+
+class WebhookServer:
+    def __init__(self, server, secret, logger=None):
+        self._server = server
+        self._secret = secret
+
+        self.logger = logger or logging.getLogger(__name__)
+
+    def create_hook(self, callback_url, get_raw=False, hook_id=None):
+        hook_url = self._build_create_hook_url()
+        params = {"callbackURL": callback_url, "getRaw": get_raw}
+        if hook_id:
+            params["hookID"] = hook_id
+
+        params["checksum"] = checksum("hooks/create", urllib.parse.urlencode(params), self._secret)
+
+        r = None
+        try:
+            r = requests.get(hook_url, params=params)
+        except requests.exceptions.ConnectionError as err:
+            self.logger.warn(f"Connection error with '{hook_url}'.")
+
+            raise HookCreateError from err
+        except Exception as err:
+            self.logger.warn(f"An unexpected error occurred with '{hook_url}'.")
+
+            raise HookCreateError from err
+        else:
+            success = parse_response(r)
+
+            if success:
+                self.logger.info(f"Webhook registered successfuly to '{self._server}'.")
+            else:
+                self.logger.info(f"Webhook registration to '{self._server}' failed.")
+
+                raise HookCreateError()
+
+    def _build_create_hook_url(self):
+        return urljoin(self._server, "bigbluebutton/api/hooks/create")
 
 
 class HookCreateError(Exception):
     pass
+
+
+def checksum(method, params, secret):
+    encoded_payload = method.encode("utf-8") + params.encode("utf-8") + secret.encode("utf-8")
+    sha1_payload = hashlib.sha1(encoded_payload)
+
+    return sha1_payload.hexdigest()
+
+
+def parse_response(response):
+    if response.status_code == requests.codes.ok:
+        response_xml = ElementTree.fromstring(response.text)
+        return_code = response_xml.find("returncode").text
+
+        return True if return_code == "SUCCESS" else False
+    else:
+        return False
