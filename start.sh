1c6d9dd2122c2e8526ceb47e575bb462ea1bf8b7
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2018-10-30 18:14:52 -0300

    Change worker class to gevent to avoid restarts

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ fi
 
 case "${AGGR_APP}" in
     webhook)
-        gunicorn main_webhook:app --bind=0.0.0.0:8000;;
+        gunicorn main_webhook:app --bind=0.0.0.0:8000 --worker-class gevent;;
     zabbix)
         python main_zabbix.py $@;;
     *)
