6d96fe4c5dcfff62d47a1875aaf456715e6900e4
rfcardoso07 <rafael.cardoso@inf.ufrgs.br>
2023-11-27 16:46:21 -0300

    Update publish events script

--- tests/black-box/publish-events/send_publish_events.py
+++ tests/black-box/publish-events/send_publish_events.py
@@ -1,15 +1,22 @@
 import json
 import os
-import time
-
 import psycopg2
 import requests
+import sys
+import time
+import urllib
 
 # Get Institution GUID from environment variable
 r_institution_guid = os.getenv("R_INSTITUTION_GUID")
 
 # Get Hook URL from environment variable
 hook_url = os.getenv("HOOK_URL")
+
+# Get Debug URL from environment variable
+debug_url = os.getenv("DEBUG_URL", None)
+
+# Get Auth Token from environment variable
+auth_token = os.getenv("AUTH_TOKEN")
 
 # Get Dry Run from environment variable
 dry_run = os.getenv("DRY_RUN", 1)
@@ -23,16 +30,19 @@ end_date = os.getenv("END_DATE")
 
 # Get DB connection parameters from environment variables
 db_params = {
-    "host": os.getenv("DB_HOST"),
-    "database": os.getenv("DB_NAME"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST", "localhost"),
+    "database": os.getenv("DB_NAME", "mconf"),
+    "user": os.getenv("DB_USER", "mconf"),
+    "password": os.getenv("DB_PASSWORD", "postgres"),
 }
 
+print("-----------------------------------------------------------------------------------------")
+print("VERSION 1.7.0 c")
 print("-----------------------------------------------------------------------------------------")
 print("Environment Variables collected")
 print("Institution GUID:", r_institution_guid)
 print("Hook URL:", hook_url)
+print("Auth Token:", auth_token)
 print("Dry Run:", dry_run)
 print("Query Limit:", limit)
 print("Start Date:", start_date)
@@ -86,6 +96,7 @@ try:
             WHERE r_institution_guid = %s
             AND created_at::date >= %s
             AND created_at::date <= %s
+            ORDER BY id
             LIMIT %s
             """
 
@@ -106,40 +117,45 @@ try:
         download = row[COLUMNS["download"]] if row[COLUMNS["download"]] else {}
         workflow = row[COLUMNS["workflow"]] if row[COLUMNS["workflow"]] else {}
 
-        # Create JSON object
-        json_object = {
-            "data": {
-                "type": "event",
-                "id": "rap-publish-ended",
-                "attributes": {
-                    "meeting": {
-                        "internal-meeting-id": row[COLUMNS["internal_meeting_id"]],
-                        "external-meeting-id": row[COLUMNS["external_meeting_id"]]
+        for item in playback:
+            # Create JSON object
+            json_object = {
+                "data":{
+                    "type":"event",
+                    "id":"rap-publish-ended",
+                    "attributes":{
+                        "meeting":{
+                            "internal-meeting-id":row[COLUMNS["internal_meeting_id"]],
+                            "external-meeting-id":row[COLUMNS["external_meeting_id"]]
+                        },
+                        "record-id":row[COLUMNS["record_id"]],
+                        "success":True,
+                        "step-time":0,
+                        "workflow":item["format"],
+                        "recording":{
+                            "name":row[COLUMNS["name"]],
+                            "is-breakout":row[COLUMNS["is_breakout"]],
+                            "start-time":row[COLUMNS["start_time"]],
+                            "end-time":row[COLUMNS["end_time"]],
+                            "size":row[COLUMNS["size"]],
+                            "raw-size":row[COLUMNS["raw_size"]],
+                            "metadata":metadata,
+                            "playback":item,
+                            "download":download
+                        }
                     },
-                    "record-id": row[COLUMNS["record_id"]],
-                    "success": True,
-                    "step-time": row[COLUMNS["end_time"]],    # Check later
-                    "workflow": workflow,
-                    "recording": {
-                        "name": row[COLUMNS["name"]],
-                        "is-breakout": row[COLUMNS["is_breakout"]],
-                        "size": row[COLUMNS["size"]],
-                        "metadata": metadata,
-                        "playback": playback,
-                        "download": download
+                    "event":{
+                        "ts":row[COLUMNS["end_time"]]
                     }
-                },
-                "event": {
-                    "ts": row[COLUMNS["end_time"]]    # Check later
                 }
             }
-        }
 
-        # Append the formatted JSON object to the list
-        json_objects.append(json_object)
+            # Append tuple (Record ID, Playback Format and JSON object) to the list
+            json_objects.append((row[COLUMNS["record_id"]], item["format"], json_object))
 
 except psycopg2.Error as error:
     print("Error connecting to the PostgreSQL database:", error)
+    sys.exit(1)
 
 finally:
     # Close the cursor and connection
@@ -150,21 +166,38 @@ finally:
 
 if dry_run == 0 or dry_run == '0': # Dry Run is explicitly set as off
     # Send events to Hook
+    auth_headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
     ok_events = 0
     not_ok_events = 0
     for object in json_objects:
+        print(f"Sending event for Record ID {object[0]} with Playback Format {object[1]}.")
+        payload = {
+            "event":json.dumps(obj=[object[2]], separators=(',', ':')),
+            "timestamp":(object[2])["data"]["event"]["ts"],
+            "domain":"mconf-worker-aws-prd"
+        }
+        encoded_payload = urllib.parse.urlencode(payload, quote_via=urllib.parse.quote)
+
+        if debug_url != None:
+            try:
+                debug_response = requests.post(url=debug_url, data=encoded_payload, headers=auth_headers)
+            except Exception as error:
+                print("Error sending event to Debug Hook:", str(error))
+
         try:
-            print("Sending event...")
-            hook_response = requests.post(hook_url, json=object)
+            hook_response = requests.post(url=hook_url, data=encoded_payload, headers=auth_headers)
         except Exception as error:
-            print("Error sending event:", error)
+            print("Error sending event:", str(error))
             not_ok_events += 1
         else:
             if hook_response.ok:
                 print("Hook response OK")
                 ok_events += 1
             else:
-                print("Hook response NOT OK:", hook_response.raise_for_status())
+                print("Hook response NOT OK, Status Code:", hook_response.status_code)
                 not_ok_events += 1
 
     print("Successful events:", ok_events)
@@ -173,6 +206,6 @@ if dry_run == 0 or dry_run == '0': # Dry Run is explicitly set as off
 
 else:
     # Print the formatted JSON objects
-    print("Printing builded JSON events...")
+    print("Printing built JSON events...")
     print(json.dumps(json_objects, indent=4))
     print("Total Recordings:", len(json_objects))
